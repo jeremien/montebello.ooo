71914546df87c28efb8ba7cd1a47f16fa5c67536
Jérémie Nuel <jeremie.nuel@pm.me>
2021-12-13 16:14:00 +0100

    generate

---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="item">
+        <p>test</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+export default {
+  name: 'IndexPage',
+  components: {
+    Header
+  }
+}
+</script>
