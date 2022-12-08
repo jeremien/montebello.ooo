0deee70d432f85289b31c3f8872379839a35849e
Jérémie Nuel <jeremie.nuel@pm.me>
2022-12-08 11:37:54 +0100

    corr site juliette

--- components/Droite.vue
+++ components/Droite.vue
@@ -5,7 +5,7 @@
         </div>
         <div class="resident">
             <p>Les résident·e·s:<br>
-                <a href="http://margauxauria.fr/">Margaux Auria</a><br>
+                <a href="https://www.instagram.com/margaux_auria/">Margaux Auria</a><br>
                 <a href="https://merrylbouchereau.com/">Merryl Bouchereau</a><br>
                 <a href="https://www.geraldinegrammon.com/">Géraldine Grammon</a><br>
                 <a href="http://helenehulak.com/">Hélène Hulak</a><br> 
@@ -14,7 +14,9 @@
                 <a href="https://melissamariller.com/">Mélissa Mariller</a><br> 
                 <a href="https://jeremienuel.fr/">Jérémie Nuel</a><br>
                 <a href="https://www.etsy.com/fr/shop/FogharStudio">Studio Foghar (Donia Ouertani et Nolwenn Michéa)</a><br>             
-                <a href="https://studioganek.com/">Studio Ganek</a><br>
+                <a href="https://www.juliette-montier.com/">Juliette Montier</a><br>  
+                <a href="https://studioganek.com/">Studio Ganek</a><br>    
+                <a href="https://www.hugoziegler.fr/">Hugo Ziegler</a>
             </p>
         </div>
     </div>
